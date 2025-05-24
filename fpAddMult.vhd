b4015737c1529485697e5eec51a7b7c58fdfb2b3
shukla <destroyernv2004@gmail.com>
2025-05-24 01:26:57 -0400

    stuf

--- fpAddMult.vhd
+++ fpAddMult.vhd
@@ -4,9 +4,9 @@ USE ieee.std_logic_1164.ALL;
 
 ENTITY fpAddMult IS
 	PORT (
-		GClock, GReset, SignA, SignB : IN STD_LOGIC;
-		MantissaA, MantissaB : IN STD_LOGIC_VECTOR(7 downto 0);
-		ExponentA, ExponentB : IN STD_LOGIC_VECTOR(6 downto 0);   
+		GClock, GReset, SignIn, selAB, startOpr : IN STD_LOGIC;
+		MantissaIn : IN STD_LOGIC_VECTOR(7 downto 0);
+		ExponentIn : IN STD_LOGIC_VECTOR(6 downto 0);   
 		SignOut, Overflow, SignsOpposite : OUT STD_LOGIC;
 		MantissaOut : OUT STD_LOGIC_VECTOR(7 downto 0);
 		ExponentOut : OUT STD_LOGIC_VECTOR(6 downto 0)
@@ -14,86 +14,150 @@ ENTITY fpAddMult IS
 END fpAddMult; 
 	
 architecture basic of fpAddMult is
-	signal int_isNegative : STD_LOGIC;
-	signal shiftRightOneMaybe : STD_LOGIC_VECTOR (2 downto 0);
-	signal negative127 : STD_LOGIC_VECTOR (8 downto 0) := "10000001";
-
-	component enardFF_2 is
-		port(
-			i_resetBar	: in std_logic;
-			i_d		   	: in std_logic;
-			i_enable   	: in std_logic;
-			i_clock	   	: in std_logic;
-			o_q,o_qBar	: out std_logic;
-		);
-	end component;
-
-	component decrementer_8bit IS
+	
+	
+	component fpAdd is
 		PORT (
-			data_in  	: IN  STD_LOGIC_VECTOR(7 downto 0);  	-- 8-bit input data
-			GClock 		: IN  STD_LOGIC; 
-			i_enable 	: IN  STD_LOGIC; 
-			GReset 		: IN  STD_LOGIC; 
-			i_load 		: IN  STD_LOGIC;  						-- load control added
-			data_out 	: OUT STD_LOGIC_VECTOR(7 downto 0)   	-- 8-bit output data
+			GClock, GReset, SignA, SignB : IN STD_LOGIC;
+			MantissaA, MantissaB : IN STD_LOGIC_VECTOR(7 downto 0);
+			ExponentA, ExponentB : IN STD_LOGIC_VECTOR(6 downto 0);
+			SignOut, Overflow : OUT STD_LOGIC;
+			MantissaOut : OUT STD_LOGIC_VECTOR(7 downto 0);
+			ExponentOut : OUT STD_LOGIC_VECTOR(6 downto 0)
 		);
 	end component;
-
+	
+	component fpMult is
+		PORT (
+			GClock, GReset, SignA, SignB : IN STD_LOGIC;
+			MantissaA, MantissaB : IN STD_LOGIC_VECTOR(7 downto 0);
+			ExponentA, ExponentB : IN STD_LOGIC_VECTOR(6 downto 0);
+			SignOut, Overflow : OUT STD_LOGIC;
+			MantissaOut : OUT STD_LOGIC_VECTOR(7 downto 0);
+			ExponentOut : OUT STD_LOGIC_VECTOR(6 downto 0)
+		);
+	end component;
+	
+	component mux_2to1_8bit IS
+		PORT (
+			sel     : IN  STD_LOGIC;                             -- Select input
+         d_in1   : IN  STD_LOGIC_VECTOR(7 downto 0);        -- 8-bit Data input 1
+         d_in2   : IN  STD_LOGIC_VECTOR(7 downto 0);        -- 8-bit Data input 2                         -- Reset input
+         d_out   : OUT STD_LOGIC_VECTOR(7 downto 0)          -- 8-bit Data output
+		);
+	end component;
+	
+	component enardFF_2 is
+      port (
+         i_resetBar : in  std_logic;
+         i_d        : in  std_logic;
+         i_enable   : in  std_logic;
+         i_clock    : in  std_logic;
+         o_q        : out std_logic;
+         o_qBar     : out std_logic
+      );
+   end component;
+		
+	SIGNAL transientManA, transientManB : STD_LOGIC_VECTOR(7 downto 0);
+	SIGNAL transientExpA, transientExpB : STD_LOGIC_VECTOR(6 downto 0);
+	SIGNAL enableManB, transSignA, transSignB, sticky, dff_in, startCondition : STD_LOGIC;
 begin
-	tempExpA <= '0' & ExponentA;
-	tempExpB <= '0' & ExponentB;
 
-	exponentSum: CLA_8bit
-		PORT MAP (
-			a => tempExpA,
-			b => tempExpB,
-			cin => open,
-			Sum => tempSum,
-			CarryOut => open,
-			zeroOut => open,
-			OverFlowOut => open
-		);
 	
-	subExponent: CLA_8bit
-		PORT MAP (
-			a => tempSum,
-			b => negative127,
-			cin => open,
-			Sum => tempSum,
-			CarryOut => open,
-			zeroOut => open,
-			OverFlowOut => open
-		);
+	enableManB <= NOT selAB;
 	
-	multiplyMantissas: CLA_8bit
-		PORT MAP(
-			a => tempManSum,
-			b => MantissaA,
-			cin => open,
-			Sum => tempManSum,
-			CarryOut => open,
-			zeroOut => open,
-			OverFlowOut => open
-		);
 	
-	signsOpposite <= MantissaA[7] XOR MantissaB[7];
-
-	barrel_shifter_shift_right_one: barrel_shifter_8bit
+	dff_signa: enardFF_2 port map(
+            i_resetBar => GReset,
+            i_d        => SignIn,
+            i_enable   => selAB,
+            i_clock    => GClock,
+            o_q        => transSignA,
+            o_qBar     => open
+        );
+		  
+	dff_signb: enardFF_2 port map(
+            i_resetBar => GReset,
+            i_d        => SignIn,
+            i_enable   => enableManB,
+            i_clock    => GClock,
+            o_q        => transSignB,
+            o_qBar     => open
+        );
+	
+	gen_manA: for i in 0 to 7 generate
+        dff_mana: enardFF_2 port map(
+            i_resetBar => GReset,
+            i_d        => MantissaIn(i),
+            i_enable   => selAB,
+            i_clock    => GClock,
+            o_q        => transientManA(i),
+            o_qBar     => open
+        );
+    end generate;
+	 
+	 gen_expA: for i in 0 to 6 generate
+        dff_expa: enardFF_2 port map(
+            i_resetBar => GReset,
+            i_d        => ExponentIn(i),
+            i_enable   => selAB,
+            i_clock    => GClock,
+            o_q        => transientExpA(i),
+            o_qBar     => open
+        );
+    end generate;
+	 
+	 gen_manB: for i in 0 to 7 generate
+        dff_manb: enardFF_2 port map(
+            i_resetBar => GReset,
+            i_d        => MantissaIn(i),
+            i_enable   => enableManB,
+            i_clock    => GClock,
+            o_q        => transientManB(i),
+            o_qBar     => open
+        );
+    end generate;
+	 
+	 gen_expB: for i in 0 to 6 generate
+        dff_expb: enardFF_2 port map(
+            i_resetBar => GReset,
+            i_d        => ExponentIn(i),
+            i_enable   => enableManB,
+            i_clock    => GClock,
+            o_q        => transientExpB(i),
+            o_qBar     => open
+        );
+    end generate;
+	 
+	 dff_in <= startOpr or sticky;
+	 
+	 dff_sticky: enardFF_2 port map(
+            i_resetBar => GReset,
+            i_d        => dff_in,
+            i_enable   => '1',
+            i_clock    => GClock,
+            o_q        => sticky,
+            o_qBar     => open
+        );
+	 
+	 startCondition <= GReset AND NOT sticky;
+	 
+	 
+	 doAdd: fpAdd
 		PORT MAP (
-			A => tempManSum,
-			S => shiftrightonemaybe,
-			Y => rightshiftedMantissa
-		);
-
-	dec: decrementer_8bit
-		PORT MAP (
-			data_in => tempExp,
 			GClock => GClock,
-			i_enable => shouldStop,
-			GReset => GReset,
-			i_load => startShifting,
-			data_out => intExponentOut
+			GReset => startCondition,
+			SignA => transSignA,
+			SignB => transSignB,
+			MantissaA => transientManA,
+			MantissaB => transientManB,
+			ExponentA => transientExpA,
+			EXponentB => transientExpB,
+			SignOut => SignOut,
+			MantissaOut => MantissaOut,
+			ExponentOut => ExponentOut,
+			Overflow => Overflow
 		);
+	 
 	
-	ExponentOut <= intExponentOut;
-	MantissaOut <= rightshiftedMantissa;
+end basic;
